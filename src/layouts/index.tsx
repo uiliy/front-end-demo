0fed2347ff65d71ac2d87bdb60cc27e16cee2a0b
uiliy <yiliuzhen@hotmail.com>
2021-01-11 17:50:22 +0800

    给全局布局添加样式

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import cn from 'antd/es/locale/zh_CN';
+import styles from './index.less';
 
 const BasicLayout: React.FC<{}> = ({ children }) => {
-  return <ConfigProvider locale={cn}>{children}</ConfigProvider>;
+  return (
+    <ConfigProvider locale={cn}>
+      <div className={styles.layout}>{children}</div>
+    </ConfigProvider>
+  );
 };
 
 export default BasicLayout;
