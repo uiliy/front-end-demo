08a5267a4c4766d4ce640573f6be424f4bea09ea
uiliy <yiliuzhen@hotmail.com>
2021-01-11 19:54:48 +0800

    新建 UserTable

---
+++ src/pages/components/UserTable.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Table, Divider } from 'antd';
+
+const dataSource = [
+  {
+    key: '1',
+    name: '胡彦斌',
+    age: 32,
+    address: '西湖区湖底公园1号',
+  },
+  {
+    key: '2',
+    name: '胡彦祖',
+    age: 42,
+    address: '西湖区湖底公园1号',
+  },
+];
+
+const columns = [
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: '住址',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: '操作',
+    key: 'actions',
+    render: (text: any, record: any) => (
+      <div>
+        <a>删除</a>
+        <Divider type="vertical" />
+        <a>编辑</a>
+      </div>
+    ),
+  },
+];
+
+const UserTable: React.FC<{}> = ({}) => {
+  return <Table dataSource={dataSource} columns={columns} />;
+};
+
+export default UserTable;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './index.less';
+import UserTable from '@/pages/components/UserTable';
 
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Hello World</h1>
+      <UserTable />
     </div>
   );
 };
