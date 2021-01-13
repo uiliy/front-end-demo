ace94eb8e0490baccfe3cc48e04c3d27ba5591ee
uiliy <yiliuzhen@hotmail.com>
2021-01-13 15:26:28 +0800

    使用 useState 保存变量

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'dva';
 import UserTable from '@/pages/components/UserTable';
+import { Row, Col, Input } from 'antd';
 
 const Users: React.FC<{ dispatch: any; users: any }> = ({
   dispatch,
   users,
 }) => {
+  const [searchValue, setSearchValue] = useState('');
+
   const handleDeleteUser = (userId: any) => {
     dispatch({
       type: 'users/deleteUser',
@@ -13,9 +16,29 @@ const Users: React.FC<{ dispatch: any; users: any }> = ({
     });
   };
 
+  const handleChange = (e: any) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div>
-      <UserTable dataSource={users} handleDeleteUser={handleDeleteUser} />
+      <Row gutter={[0, 16]}>
+        <Col span={10}>
+          <Input.Search
+            placeholder="模糊搜索"
+            autoFocus
+            allowClear
+            enterButton
+            onChange={handleChange}
+          ></Input.Search>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={24}>
+          <UserTable dataSource={users} handleDeleteUser={handleDeleteUser} />
+        </Col>
+      </Row>
     </div>
   );
 };
